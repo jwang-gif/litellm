7a89d9fb463870b2743f95086c37abd86e5182e0
Joy Wang <j.wang@zscaler.com>
2025-09-23 16:02:55 -0700

    add doc sidebar

--- docs/my-website/sidebars.js
+++ docs/my-website/sidebars.js
@@ -50,6 +50,7 @@ const sidebars = {
           "proxy/guardrails/custom_guardrail",
           "proxy/guardrails/prompt_injection",
           "proxy/guardrails/tool_permission",
+          "proxy/guardrails/zscaler_ai_guard",
         ].sort(),
       ],
     },
